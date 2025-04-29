b4887e271a60e811dec7184c6003b8057cdec344
Kols <damolausmankolapo@gmail.com>
2025-04-29 10:54:02 +0100

    add images to images array

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./scss/main.scss";
+const imageGrid = document.querySelector(".image__grid");
+const scrollable = document.querySelector(".scrollable");
+let images = [];
+
+let currentY = 0;
+let currentX = 0;
+
+// Add images
+for (let i = 0; i < 10; i++) {
+  let div = document.createElement("div");
+  div.classList.add("image__container");
+  let image = document.createElement("img");
+  image.src = `./images/${i + 1}.jpeg`;
+}
